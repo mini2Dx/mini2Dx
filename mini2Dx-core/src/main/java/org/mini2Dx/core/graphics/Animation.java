f88e200e10bb21072e49a06e197934cfbdb1474e
Thomas Cashman <thebattlebard@me.com>
2013-02-22 14:13:56 +0000

    Adds animation class implementation.

--- mini2Dx-core/src/main/java/org/mini2Dx/core/graphics/Animation.java
+++ mini2Dx-core/src/main/java/org/mini2Dx/core/graphics/Animation.java
@@ -1,8 +1,8 @@
 package org.mini2Dx.core.graphics;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 /**
@@ -10,33 +10,50 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
  * 
  * @author Thomas Cashman
  */
-public class Animation {
-	private List<Texture> frameTextures;
-	private List<TextureRegion> frameTextureRegions;
+public class Animation<T extends TextureRegion> {
+	private List<T> frames;
+	private List<Float> durations;
 	private int currentFrame;
+	private float elapsed;
 	
+	/**
+	 * Constructor
+	 */
 	public Animation() {
 		currentFrame = 0;
+		frames = new ArrayList<T>();
+		durations = new ArrayList<Float>();
+		elapsed = 0f;
 	}
 	
+	/**
+	 * Adds a frame to the animation
+	 * @param frame The frame to be added
+	 * @param duration The duration of the frame in seconds
+	 */
+	public void addFrame(T frame, float duration) {
+		durations.add(duration);
+		frames.add(frame);
+	}
+	
+	/**
+	 * Updates the animation
+	 * @param delta
+	 */
 	public void update(float delta) {
-		
+		elapsed += delta;
+		if(elapsed > durations.get(currentFrame)) {
+			elapsed = 0f;
+			currentFrame = currentFrame == frames.size() - 1 ? 0 : currentFrame + 1;
+		}
 	}
 	
 	public void render(Graphics g, int x, int y) {
-		
+		g.drawTextureRegion(frames.get(currentFrame), x, y);
 	}
 	
 	public int getNumberOfFrames() {
-		if(frameTextures == null && frameTextureRegions == null) {
-			return 0;
-		}
-		
-		if(frameTextures != null) {
-			return frameTextures.size();
-		} else {
-			return frameTextureRegions.size();
-		}
+		return frames.size();
 	}
 	
 	public int getCurrentFrame() {
