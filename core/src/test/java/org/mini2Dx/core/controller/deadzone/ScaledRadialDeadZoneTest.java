4b049967f09551c615cb1f9a9e18665f12c33c0d
Thomas Cashman <thomascashman404@gmail.com>
2016-06-29 12:25:29 +0200

    Added controller dead zone implementations

---
+++ core/src/test/java/org/mini2Dx/core/controller/deadzone/ScaledRadialDeadZoneTest.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2016 See AUTHORS file
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of the mini2Dx nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.mini2Dx.core.controller.deadzone;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+/**
+ * Unit tests for {@link ScaledRadialDeadZone}
+ */
+public class ScaledRadialDeadZoneTest {
+	private final ScaledRadialDeadZone scaledRadialDeadZone = new ScaledRadialDeadZone();
+	
+	@Test
+	public void testInsideDeadZone() {
+		scaledRadialDeadZone.setDeadZone(0.25f);
+		scaledRadialDeadZone.updateX(0.1f);
+		scaledRadialDeadZone.updateY(0.1f);
+		Assert.assertEquals(0f, scaledRadialDeadZone.getX());
+		Assert.assertEquals(0f, scaledRadialDeadZone.getY());
+	}
+	
+	@Test
+	public void testXInsideDeadZone() {
+		scaledRadialDeadZone.setDeadZone(0.25f);
+		scaledRadialDeadZone.updateX(0.1f);
+		scaledRadialDeadZone.updateY(0.3f);
+		Assert.assertEquals(true, scaledRadialDeadZone.getX() > 0f);
+		Assert.assertEquals(true, scaledRadialDeadZone.getY() > 0f);
+	}
+	
+	@Test
+	public void testYInsideDeadZone() {
+		scaledRadialDeadZone.setDeadZone(0.25f);
+		scaledRadialDeadZone.updateX(0.3f);
+		scaledRadialDeadZone.updateY(0.1f);
+		Assert.assertEquals(true, scaledRadialDeadZone.getX() > 0f);
+		Assert.assertEquals(true, scaledRadialDeadZone.getY() > 0f);
+	}
+	
+	@Test
+	public void testOutsideDeadZone() {
+		scaledRadialDeadZone.setDeadZone(0.25f);
+		scaledRadialDeadZone.updateX(0.3f);
+		scaledRadialDeadZone.updateY(0.3f);
+		Assert.assertEquals(true, scaledRadialDeadZone.getX() > 0f);
+		Assert.assertEquals(true, scaledRadialDeadZone.getY() > 0f);
+	}
+}
