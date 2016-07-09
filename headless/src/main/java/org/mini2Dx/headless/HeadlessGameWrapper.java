f9eaba5b8abe160ae7c41f562c28e29d7ce1a3ed
paul <paul@hyperverse.net>
2016-07-09 17:54:58 +0200

    Added headless backend

---
+++ headless/src/main/java/org/mini2Dx/headless/HeadlessGameWrapper.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2015 See AUTHORS file
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of the mini2Dx nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.mini2Dx.headless;
+
+import org.mini2Dx.core.Mdx;
+import org.mini2Dx.core.game.GameContainer;
+import org.mini2Dx.core.game.GameWrapper;
+import org.mini2Dx.core.graphics.Graphics;
+import org.mini2Dx.core.graphics.HeadlessGraphics;
+import org.mini2Dx.desktop.di.DesktopDependencyInjection;
+import org.mini2Dx.desktop.playerdata.DesktopPlayerData;
+import org.mini2Dx.desktop.serialization.DesktopXmlSerializer;
+
+/**
+ * Desktop implementation of {@link GameWrapper}
+ */
+public class HeadlessGameWrapper extends GameWrapper {
+
+	public HeadlessGameWrapper(GameContainer gc, String gameIdentifier) {
+		super(gc, gameIdentifier);
+	}
+
+	@Override
+	public void initialise(String gameIdentifier) {
+		Mdx.xml = new DesktopXmlSerializer();
+		Mdx.di = new DesktopDependencyInjection();
+		Mdx.playerData = new DesktopPlayerData(gameIdentifier);
+	}
+
+	@Override
+	protected Graphics createGraphicsContext() {
+		return new HeadlessGraphics();
+	}
+	
+}
