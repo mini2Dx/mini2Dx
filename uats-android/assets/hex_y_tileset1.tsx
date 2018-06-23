2a324b235d04dd03e2ef54fa25b2ff208c11a535
Thomas Cashman <thomascashman404@gmail.com>
2018-06-23 13:47:55 +0200

    Initial project set up for 2.0

--- uats-android/assets/hex_y_tileset1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="0" tilewidth="60" tileheight="80" tilecount="12" columns="4">
- <image source="hexagonal_y_tileset.png" trans="000000" width="240" height="240"/>
-</tileset>
