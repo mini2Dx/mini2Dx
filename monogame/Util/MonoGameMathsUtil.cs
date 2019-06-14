0fc3267c34066767167aee4ef10122bd7d912f23
Augusto Zanellato <augusto.zanellato@gmail.com>
2019-06-14 09:23:50 +0100

    Implemented Viewport related APIs (except for Viewports themselves)
    Below there are 11 squashed commit messages:
    
    Optimized pixmap rendering functions by caching the color in rgba8888 format
    Implemented translation methods in MonoGameGraphics
    Implemented scaling methods in MonoGameGraphics
    Now you can clear a pixmap in O(1) (but it may cause problems with GC because it allocates a new array each time you clear)
    Renamed methods in MonoGameMathsUtil because their name was wrong
    Optimized MonoGameGraphics Vector2 usage, now they are allocated only once.
    Fixed MonoGameGraphics.flush()
    Now MonoGameGraphics.preRender(int, int) clears the screen
    The pixmap used as the MonoGameGraphics shape renderer backend is now all transparent because the background color is handled via MonoGame APIs
    Changed UAT background
    Improved MonoGameGraphics rotation logic

--- monogame/Util/MonoGameMathsUtil.cs
+++ monogame/Util/MonoGameMathsUtil.cs
@@ -4,11 +4,11 @@ namespace monogame.Util
 {
     public static class MonoGameMathsUtil
     {
-        public static double radianToDegree(double angle)
+        public static double degreeToRadian(double angle)
         {
             return Math.PI * angle / 180.0;
         }
-        public static float radianToDegree(float angle)
+        public static float degreeToRadian(float angle)
         {
             return (float)Math.PI * angle / 180f;
         }
