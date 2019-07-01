017e45e2e5476150feac5232d589798f159bf9fb
Augusto Zanellato <augusto.zanellato@gmail.com>
2019-07-01 12:53:29 +0200

    Implemented Logger API

---
+++ monogame/MonoGameLogger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using Exception = java.lang.Exception;
+
+namespace monogame
+{
+    class MonoGameLogger : org.mini2Dx.core.Logger
+    {
+        public void debug(string tag, string msg)
+        {
+            Debug.WriteLine($"D/[{tag}] {msg}");
+        }
+
+        public void error(string tag, string msg, Exception e)
+        {
+            Console.Error.WriteLine($"D/[{tag}] {msg}\n{e.StackTrace}");
+        }
+
+        public void error(string tag, string msg)
+        {
+            Console.Error.WriteLine($"E/[{tag}] {msg}");
+        }
+
+        public void info(string tag, string msg)
+        {
+            Console.WriteLine($"I/[{tag}] {msg}");
+        }
+    }
+}
