2ea69a51a9d2efbf0ed570f407750ee34c9c3813
Caidan Williams <caidanstevenwilliams@gmail.com>
2019-07-11 08:56:11 -0600

    Added HoverListenerAdapter to provide no-op methods

---
+++ ui/src/main/java/org/mini2Dx/ui/listener/HoverListenerAdapter.java
@@ -0,0 +1,15 @@
+package org.mini2Dx.ui.listener;
+
+import org.mini2Dx.ui.element.Hoverable;
+
+/**
+ * Adapter class that implements {@link HoverListener}. All methods are
+ * no-op and can be overridden individually.
+ */
+public class HoverListenerAdapter implements HoverListener {
+    @Override
+    public void onHoverBegin(Hoverable source) {}
+
+    @Override
+    public void onHoverEnd(Hoverable source) {}
+}
