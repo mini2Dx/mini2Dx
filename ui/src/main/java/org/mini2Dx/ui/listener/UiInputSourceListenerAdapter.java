78a9a8d16e8bed6eec0ac61901d0b782bf967551
Caidan Williams <caidanstevenwilliams@gmail.com>
2019-07-11 08:58:17 -0600

    Added UiInputSourceListenerAdapter to provide no-op methods

---
+++ ui/src/main/java/org/mini2Dx/ui/listener/UiInputSourceListenerAdapter.java
@@ -0,0 +1,17 @@
+package org.mini2Dx.ui.listener;
+
+import org.mini2Dx.core.input.GamePadType;
+import org.mini2Dx.ui.InputSource;
+import org.mini2Dx.ui.UiContainer;
+
+/**
+ * Adapter class that implements {@link UiInputSourceListener}. All methods are
+ * no-op and can be overridden individually.
+ */
+public class UiInputSourceListenerAdapter implements UiInputSourceListener {
+    @Override
+    public void inputSourceChanged(UiContainer container, InputSource oldInputSource, InputSource newInputSource) {}
+
+    @Override
+    public void gamePadTypeChanged(UiContainer container, GamePadType oldGamePadType, GamePadType newGamePadType) {}
+}
