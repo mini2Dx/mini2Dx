b527407acb8d6e99210b2a89de56f5d3b33a23d6
Caidan Williams <caidanstevenwilliams@gmail.com>
2019-07-11 08:52:16 -0600

    Added ActionListenerAdapter to provide no-op methods

---
+++ ui/src/main/java/org/mini2Dx/ui/listener/ActionListenerAdapater.java
@@ -0,0 +1,15 @@
+package org.mini2Dx.ui.listener;
+
+import org.mini2Dx.ui.event.ActionEvent;
+
+/**
+ * Adapter class that implements {@link ActionListener}. All methods are
+ * no-op and can be overridden individually.
+ */
+public class ActionListenerAdapater implements ActionListener {
+    @Override
+    public void onActionBegin(ActionEvent event) {}
+
+    @Override
+    public void onActionEnd(ActionEvent event) {}
+}
