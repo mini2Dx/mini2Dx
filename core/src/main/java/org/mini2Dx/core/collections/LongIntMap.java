ca577497d8ff3dfb35856d37d3d874636bb648da
Thomas Cashman <thomas.cashman@viridiansoftware.com>
2021-05-22 14:39:02 +0100

    Fix javadoc errors

--- core/src/main/java/org/mini2Dx/core/collections/LongIntMap.java
+++ core/src/main/java/org/mini2Dx/core/collections/LongIntMap.java
@@ -89,7 +89,7 @@ public class LongIntMap implements Iterable<LongIntMap.Entry> {
 		return tableSize;
 	}
 
-	/** Returns an index >= 0 and <= {@link #mask} for the specified {@code item}.
+	/** Returns an index &gt;= 0 and &lt;= {@link #mask} for the specified {@code item}.
 	 * <p>
 	 * The default implementation uses Fibonacci hashing on the item's {@link Object#hashCode()}: the hashcode is multiplied by a
 	 * long constant (2 to the 64th, divided by the golden ratio) then the uppermost bits are shifted into the lowest positions to
