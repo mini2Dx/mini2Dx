mini2Dx/mini2Dx

commit aecfb5aa20abab5c437e5e7c7442e4f3f7c90b6f

Updated UiContainer to set last input source on key down
Updated UiContainer to set last input to Keyboard_Mouse on Key Down.
Added test case.

ui/src/main/java/org/mini2Dx/ui/UiContainer.java
@@ -479,6 +479,7 @@ public class UiContainer extends ParentUiElement implements InputProcessor {
 
 	@Override
 	public boolean keyDown(int keycode) {
+		setLastInputSource(InputSource.KEYBOARD_MOUSE);
 		if (activeTextInput != null && activeTextInput.isReceivingInput()) {
 			receivedKeyDowns.add(keycode);
 			return true;

ui/src/test/java/org/mini2Dx/ui/UiContainerTest.java
@@ -88,6 +88,17 @@ public class UiContainerTest implements UiContainerListener {
 		uiContainer.update(1f);
 		Assert.assertEquals(true, listenerEvents.contains("inputSourceChanged"));
 	}
+
+	@Test
+	public void testKeyDownChangeInputSource() {
+		uiContainer.setLastInputSource(InputSource.CONTROLLER);
+		uiContainer.update(1f);
+
+		Assert.assertEquals(InputSource.CONTROLLER, uiContainer.getLastInputSource());
+		uiContainer.keyDown(Keys.A);
+		uiContainer.update(1f);
+		Assert.assertEquals(InputSource.KEYBOARD_MOUSE, uiContainer.getLastInputSource());
+	}
 	
 	@Test
 	public void testDeferredControllerTypeChangeNotification() {

commit fd60a4bbfbbf960e4a3a3d87e23d59420b82d67e

Fix controller UI navigation regression caused by aecfb5aa2

ui/src/main/java/org/mini2Dx/ui/UiContainer.java
@@ -480,6 +480,14 @@ public class UiContainer extends ParentUiElement implements InputProcessor {
 	@Override
 	public boolean keyDown(int keycode) {
 		setLastInputSource(InputSource.KEYBOARD_MOUSE);
+		return keyDownNoInputChange(keycode);
+	}
+
+	/**
+	 * Simulates a key down event without changing the input source
+	 * @param keycode
+	 */
+	public boolean keyDownNoInputChange(int keycode) {
 		if (activeTextInput != null && activeTextInput.isReceivingInput()) {
 			receivedKeyDowns.add(keycode);
 			return true;

ui/src/main/java/org/mini2Dx/ui/gamepad/PS4UiInput.java
@@ -66,7 +66,7 @@ public class PS4UiInput extends PS4GamePadAdapter implements GamePadUiInput<PS4B
 		
 		if(left) {
 			if(leftTimer == stickRepeatTimer) {
-				uiContainer.keyDown(Keys.LEFT);
+				uiContainer.keyDownNoInputChange(Keys.LEFT);
 				uiContainer.keyUp(Keys.LEFT);
 			}
 			leftTimer -= delta;
@@ -79,7 +79,7 @@ public class PS4UiInput extends PS4GamePadAdapter implements GamePadUiInput<PS4B
 		
 		if(right) {
 			if(rightTimer == stickRepeatTimer) {
-				uiContainer.keyDown(Keys.RIGHT);
+				uiContainer.keyDownNoInputChange(Keys.RIGHT);
 				uiContainer.keyUp(Keys.RIGHT);
 			}
 			rightTimer -= delta;
@@ -92,7 +92,7 @@ public class PS4UiInput extends PS4GamePadAdapter implements GamePadUiInput<PS4B
 		
 		if(up) {
 			if(upTimer == stickRepeatTimer) {
-				uiContainer.keyDown(Keys.UP);
+				uiContainer.keyDownNoInputChange(Keys.UP);
 				uiContainer.keyUp(Keys.UP);
 			}
 			upTimer -= delta;
@@ -105,7 +105,7 @@ public class PS4UiInput extends PS4GamePadAdapter implements GamePadUiInput<PS4B
 		
 		if(down) {
 			if(downTimer == stickRepeatTimer) {
-				uiContainer.keyDown(Keys.DOWN);
+				uiContainer.keyDownNoInputChange(Keys.DOWN);
 				uiContainer.keyUp(Keys.DOWN);
 			}
 			downTimer -= delta;
@@ -172,13 +172,13 @@ public class PS4UiInput extends PS4GamePadAdapter implements GamePadUiInput<PS4B
 		if(navigateWithDPad) {
 			switch (button) {
 			case UP:
-				return uiContainer.keyDown(Keys.UP);
+				return uiContainer.keyDownNoInputChange(Keys.UP);
 			case DOWN:
-				return uiContainer.keyDown(Keys.DOWN);
+				return uiContainer.keyDownNoInputChange(Keys.DOWN);
 			case LEFT:
-				return uiContainer.keyDown(Keys.LEFT);
+				return uiContainer.keyDownNoInputChange(Keys.LEFT);
 			case RIGHT:
-				return uiContainer.keyDown(Keys.RIGHT);
+				return uiContainer.keyDownNoInputChange(Keys.RIGHT);
 			}
 		}
 

ui/src/main/java/org/mini2Dx/ui/gamepad/XboxUiInput.java
@@ -80,7 +80,7 @@ public class XboxUiInput extends XboxGamePadAdapter implements GamePadUiInput<Xb
 		
 		if(left) {
 			if(leftTimer == stickRepeatTimer) {
-				uiContainer.keyDown(Keys.LEFT);
+				uiContainer.keyDownNoInputChange(Keys.LEFT);
 				uiContainer.keyUp(Keys.LEFT);
 				debugLog("Navigate left");
 			}
@@ -94,7 +94,7 @@ public class XboxUiInput extends XboxGamePadAdapter implements GamePadUiInput<Xb
 		
 		if(right) {
 			if(rightTimer == stickRepeatTimer) {
-				uiContainer.keyDown(Keys.RIGHT);
+				uiContainer.keyDownNoInputChange(Keys.RIGHT);
 				uiContainer.keyUp(Keys.RIGHT);
 				debugLog("Navigate right");
 			}
@@ -108,7 +108,7 @@ public class XboxUiInput extends XboxGamePadAdapter implements GamePadUiInput<Xb
 		
 		if(up) {
 			if(upTimer == stickRepeatTimer) {
-				uiContainer.keyDown(Keys.UP);
+				uiContainer.keyDownNoInputChange(Keys.UP);
 				uiContainer.keyUp(Keys.UP);
 				debugLog("Navigate up");
 			}
@@ -122,7 +122,7 @@ public class XboxUiInput extends XboxGamePadAdapter implements GamePadUiInput<Xb
 		
 		if(down) {
 			if(downTimer == stickRepeatTimer) {
-				uiContainer.keyDown(Keys.DOWN);
+				uiContainer.keyDownNoInputChange(Keys.DOWN);
 				uiContainer.keyUp(Keys.DOWN);
 				debugLog("Navigate down");
 			}
@@ -191,13 +191,13 @@ public class XboxUiInput extends XboxGamePadAdapter implements GamePadUiInput<Xb
 		if(navigateWithDPad) {
 			switch(button) {
 			case UP:
-				return uiContainer.keyDown(Keys.UP);
+				return uiContainer.keyDownNoInputChange(Keys.UP);
 			case DOWN:
-				return uiContainer.keyDown(Keys.DOWN);
+				return uiContainer.keyDownNoInputChange(Keys.DOWN);
 			case LEFT:
-				return uiContainer.keyDown(Keys.LEFT);
+				return uiContainer.keyDownNoInputChange(Keys.LEFT);
 			case RIGHT:
-				return uiContainer.keyDown(Keys.RIGHT);
+				return uiContainer.keyDownNoInputChange(Keys.RIGHT);
 			}
 		}
 